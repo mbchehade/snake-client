b6502d16f6551743d9844abb1d5ba746a3dd1981
moe <mbchehade@gmail.com>
2020-01-15 22:42:16 +0000

    added setupinput and handleuserinput

--- play.js
+++ play.js
@@ -7,3 +7,25 @@ const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", (data) => {
+    handleUserInput(data);
+  });
+  return stdin;
+}
+setupInput();
+
+//when i get data i want to pass this function
+
+const handleUserInput = ((data)=>{
+//should check ctrl + c input and terminate game.
+}) 
