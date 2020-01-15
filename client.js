5e7dd71bfdf230ff669de3c17d09fad33e31c236
moe <mbchehade@gmail.com>
2020-01-15 21:37:00 +0000

    added a message when connection establshed and then added a name

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server")
+    conn.write("Name: MBC")
+  });
+  
+ 
+  conn.on('data', (data) => {
+    console.log('server says: ', data)
+  })
+
+  return conn;
+}
+module.exports = connect;
