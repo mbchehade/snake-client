ffae24684ac7c7da303d9924f703dff48d4a574d
moe <mbchehade@gmail.com>
2020-01-15 23:03:14 +0000

    exported setupinput function

---
+++ input.js
@@ -0,0 +1,7 @@
+const setupInput = require("./play.js")
+
+//when i get data i want to pass this function
+
+const handleUserInput = ((data) => {
+  //should check ctrl + c input and terminate game.
+}) 
