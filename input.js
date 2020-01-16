070b625763c5c10ca7b220cacfeb5bd7c7118870
moe <mbchehade@gmail.com>
2020-01-16 01:27:17 +0000

    added conditions for controle

--- input.js
+++ input.js
@@ -1,7 +1,42 @@
-const setupInput = require("./play.js")
-
 //when i get data i want to pass this function
+let connection;
+
+
+
 
 const handleUserInput = ((data) => {
-  //should check ctrl + c input and terminate game.
-}) 
+  if (data === '\u0003') {
+    console.log("Thanks for using me, ciao!")
+    process.exit()
+  }
+  if(data === "w"){
+    connection.write("Move: up")
+  }
+  if (data === "a") {
+    connection.write("Move: left")
+  }
+  if (data === "s") {
+    connection.write("Move: down")
+  }
+  if (data === "d") {
+    connection.write("Move: right")
+  }
+  
+  
+});
+
+const setupInput = function (conn) {
+  const stdin = process.stdin;
+
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput(stdin);
+  connection = conn;
+  stdin.on('data', (data) => {
+    handleUserInput(data)
+  })
+  return stdin;
+}
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -4,28 +4,17 @@ const net = require('net');
  * Establishes connection with the game server
  */
 const connect = require('./client');
+const setupInput = require('./input');
+
 
 console.log('Connecting ...');
-connect();
 
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", (data) => {
-    handleUserInput(data);
-  });
-  return stdin;
-}
-setupInput();
+
+setupInput(connect());
 
 //when i get data i want to pass this function
 
-const handleUserInput = ((data)=>{
-//should check ctrl + c input and terminate game.
-}) 
